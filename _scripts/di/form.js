02ff830f075e34467b36b6aa9b742b1e91bdca28
applecompass <applecompass@gmail.com>
2011-08-18 23:02:01 +0800

    navigation

--- _scripts/di/form.js
+++ _scripts/di/form.js
@@ -7,22 +7,33 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-(function($) {
+(function ($) {
 
     var Form = {
 
-        reset: function() {
-
+        reset: function () {
+            var params = {
+                    url: "http://192.168.1.100:3000/targets?user_id=1",
+                    type: "GET",
+                    dataType: "application/json",
+                    complete: function (data) {
+                        console.log("success");
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        console.log("error");
+                    }
+                };
+            jQuery.ajax(params);
         },
 
-        init: function() {
+        init: function () {
 
         }
     };
 
     ZM.Form = {
 
-        init: function() {
+        init: function () {
             Form.init();
 
         }
